16be28f9aaf8f13d982170dc433d0adf3842c8b9
MEHD_LAGH <85755548+LEMEHD@users.noreply.github.com>
2025-12-14 21:11:40 +0100

    Delete pdb2526mehdi-master directory

--- pdb2526mehdi-master/src/main/java/org/isfce/pid/dao/ISectionDao.java
+++
@@ -1,8 +0,0 @@
-package org.isfce.pid.dao;
-
-import org.isfce.pid.model.Section;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ISectionDao extends JpaRepository<Section, String> {
-
-}
