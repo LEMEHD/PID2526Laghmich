ae0a6acdba7b4a5cbcf9100e6f6ffcecfdf2bf1d
MEHD_LAGH <85755548+LEMEHD@users.noreply.github.com>
2025-12-29 15:13:15 +0100

    Delete pdb2526mehdi-master directory

--- pdb2526mehdi-master/src/main/java/org/isfce/pid/dao/ISupportingDocumentDao.java
+++
@@ -1,8 +0,0 @@
-package org.isfce.pid.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-import org.isfce.pid.model.SupportingDocument;
-
-
-public interface ISupportingDocumentDao extends JpaRepository<SupportingDocument, UUID> { }
